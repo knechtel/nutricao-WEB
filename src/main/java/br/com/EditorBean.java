0c3d17a3efe181b2ea7f8bde785c8c2f05298935
maiquelknechtel <maiquelknechtel@10.0.0.104>
2018-03-15 03:05:17 -0300

    add methods for service

--- src/main/java/br/com/EditorBean.java
+++ src/main/java/br/com/EditorBean.java
@@ -1,12 +1,38 @@
 package br.com;
 
-import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
 
-@ManagedBean(name = "editor")
-public class EditorBean {
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Default;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+
+
+
+@RequestScoped
+@Named("editor")
+public class EditorBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	private String value = "This editor is provided by PrimeFaces";
 
+	
+	private String test;
+
+	
+	
+	public EditorBean() {
+
+	}
 	public String getValue() {
 		return value;
 	}
@@ -14,4 +40,17 @@ public class EditorBean {
 	public void setValue(String value) {
 		this.value = value;
 	}
+
+
+
+
+
+	public String getTest() {
+		return "fir ";
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+	
 }
